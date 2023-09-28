bfec75f9a0f6679a48790f9cf4a9235cd07ad197
Manoo07 <manoharboinapalli1196@gmail.com>
2023-09-28 11:18:27 +0530

    Added jwt logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig')
 const apiRoutes = require('./routes/index');
 const app = express();
+const UserService = require('./services/user-service');
 // const UserRepository =require('./repository/user-repository');
 
 const prepareAndStartServer = () => {
@@ -11,7 +12,12 @@ const prepareAndStartServer = () => {
     app.use('/api',apiRoutes);
     app.listen(PORT, async () => {
         console.log('Server started : ',PORT);
-       
+        const service = new UserService();
+        // const newToken = service.createToken({email: 'manohar@admin.com',id:'1'});
+        // console.log("new token is : ",newToken);
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hbm9oYXJAYWRtaW4uY29tIiwiaWQiOiIxIiwiaWF0IjoxNjk1ODc4NTQ3LCJleHAiOjE2OTU4ODIxNDd9.Wfet_iTuedsumUlzBf54jm0IQ3aaepiaNAAUOc99F90'
+        // const response = service.verifyToken(token);
+        // console.log("Response is : ",response);
     })
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,6 @@
-const exp = require('constants');
+const jwt = require('jsonwebtoken');
 const UserRepository = require('../repository/user-repository');
+const {JWT_KEY} = require('../config/serverConfig');
 
 class UserService {
     constructor(){
@@ -11,6 +12,24 @@ class UserService {
             return user;
         } catch (error) {
             console.log("Error in service layer : ",error);
+            throw {error};
+        }
+    }
+    createToken(user){
+        try {
+            const result = jwt.sign(user,JWT_KEY,{expiresIn:'1h'});
+            return result;
+        } catch (error) {
+            console.log("Error in token creation : ",error);
+            throw {error};
+        }
+    }
+    verifyToken(token){
+        try {
+            const response = jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Error in token creation : ",error);
             throw {error};
         }
     }
